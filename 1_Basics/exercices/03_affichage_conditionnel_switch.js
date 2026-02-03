50dbb9da71e2f8a429782fa95775e0349952a0e6
ArnaudBena <arnaud.benacquista@gmail.com>
2026-02-03 16:43:02 +0100

    Exercice 3

--- 1_Basics/exercices/03_affichage_conditionnel_switch.js
+++ 1_Basics/exercices/03_affichage_conditionnel_switch.js
@@ -5,3 +5,22 @@ const nom_boutique = "La Potion Enchantée";
 const nom_sorcier = "Archibald";
 let stock_potion_soin = 10;
 let prix_potion_soin = 50;
+
+let choix = Number.parseInt(prompt("Bienvenue dans mon humble boutique Aventurier. Que veux-tu savoir ? 🤔\n1. Le nom de la boutique\n2. Le nom du Sorcier\n3. Le prix d'un potion de soin\n4. La quantité d'une potion de soin")) 
+
+switch (choix) {
+    case 1:
+        console.log(nom_boutique)
+        break
+    case 2:
+        console.log(nom_sorcier)
+        break
+    case 3:
+        console.log(prix_potion_soin)
+        break
+    case 4:
+        console.log(stock_potion_soin)
+        break
+    default:
+        console.log("Mh... Désolé aventurier, je ne comprends pas ce que tu souhaites. Refais ton choix ! 😕")
+}
