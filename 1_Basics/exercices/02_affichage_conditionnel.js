0c58810623de64ee82dc22a04afebbacd6c98363
ArnaudBena <arnaud.benacquista@gmail.com>
2026-02-03 16:29:41 +0100

    Exercice 2

--- 1_Basics/exercices/02_affichage_conditionnel.js
+++ 1_Basics/exercices/02_affichage_conditionnel.js
@@ -2,8 +2,9 @@
 
 // Variables de l'exercice précédent (modifie selon tes valeurs)
 const nom_boutique = "La Potion Enchantée";
-const boutique_ouverte = false;
-let affichageOuverture = boutique_ouverte ? "Bienvenue dans la boutique " + nom_boutique + " Aventurier ! 🎉" :
-"La boutique" + nom_boutique + "est fermée, revenez plus tard Aventurier ! 😴"
+const boutique_ouverte = true;
+let affichageOuverture = boutique_ouverte 
+    ? "Bienvenue dans la boutique " + nom_boutique + " Aventurier ! 🎉" 
+    : "La boutique" + nom_boutique + "est fermée, revenez plus tard Aventurier ! 😴"
 
 console.log(affichageOuverture)
