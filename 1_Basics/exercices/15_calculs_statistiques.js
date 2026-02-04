68b8c1c7253a82aeb4dc5db3e9100e79e697f8c8
ArnaudBena <arnaud.benacquista@gmail.com>
2026-02-04 11:29:10 +0100

    Exercice 15

--- 1_Basics/exercices/15_calculs_statistiques.js
+++ 1_Basics/exercices/15_calculs_statistiques.js
@@ -7,3 +7,18 @@ const inventaire = [
   { nom: "Potion d'endurance", prix: 40, stock: 7 },
   { nom: "Potion de force", prix: 60, stock: 3 }
 ];
+
+let valeurInventaire = 0 
+let prixMoyen = 0
+let stockTotal = 0
+
+for (const potion of inventaire) {
+  valeurInventaire += potion.prix * potion.stock
+  prixMoyen += potion.prix
+  stockTotal += potion.stock
+}
+prixMoyen /= inventaire.length
+
+console.log("La valeur totale de l'inventaire est de",valeurInventaire + " 🪙")
+console.log("La valeur moyenne de l'inventaire est de",prixMoyen + " 🪙")
+console.log("Le stock total de potion est de",stockTotal)
