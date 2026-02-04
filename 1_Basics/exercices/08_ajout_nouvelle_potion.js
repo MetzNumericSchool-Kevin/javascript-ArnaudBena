f0ca821979fc98f4d7da6aba1e8949d8f1745446
ArnaudBena <arnaud.benacquista@gmail.com>
2026-02-04 09:53:59 +0100

    Exercice 8

--- 1_Basics/exercices/08_ajout_nouvelle_potion.js
+++ 1_Basics/exercices/08_ajout_nouvelle_potion.js
@@ -2,3 +2,8 @@
 
 // Variable de l'exercice précédent (modifie selon tes valeurs)
 const potions = ["Potion de soin", "Potion de mana", "Potion d'endurance"];
+
+potions.push("Potion de Force")
+console.log(potions)
+potions.pop(potions)
+console.log(potions)
