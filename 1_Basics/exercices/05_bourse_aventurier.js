5afdd93e528a9f58db6a6ca5aceba48ed332ba0a
ArnaudBena <arnaud.benacquista@gmail.com>
2026-02-04 09:25:05 +0100

    Exercice 5

--- 1_Basics/exercices/05_bourse_aventurier.js
+++ 1_Basics/exercices/05_bourse_aventurier.js
@@ -3,3 +3,13 @@
 // Variables des exercices précédents (modifie selon tes valeurs)
 let stock_potion_soin = 10;
 let prix_potion_soin = 50;
+let argentAventurier = 500;
+let quantiteAchat = parseInt(prompt("Combien de potion souhaitez vous acheter ?"))
+
+if (argentAventurier > prix_potion_soin && quantiteAchat < stock_potion_soin) {
+    argentAventurier -= (prix_potion_soin * quantiteAchat)
+    stock_potion_soin -= quantiteAchat
+    console.log("Merci pour votre achat il vous reste " + argentAventurier + "🪙" + "\nIl reste " + stock_potion_soin + " potions de soin en stock")
+} else {
+    console.log("Achat refusé")
+}
