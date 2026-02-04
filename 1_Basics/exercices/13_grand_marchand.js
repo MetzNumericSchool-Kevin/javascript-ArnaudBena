e89d235a09bfd40f251ddf9bf3df18c8185be5ae
ArnaudBena <arnaud.benacquista@gmail.com>
2026-02-04 11:09:01 +0100

    Exercice 13

--- 1_Basics/exercices/13_grand_marchand.js
+++ 1_Basics/exercices/13_grand_marchand.js
@@ -7,3 +7,25 @@ const inventaire = [
   { nom: "Potion d'endurance", prix: 40, stock: 0 },
   { nom: "Potion de force", prix: 60, stock: 3 },
 ];
+
+let prixMax = 0
+let stockMax = 0
+let stockSuperieurA0 = 0
+let nomPotion;
+
+for (const potion of inventaire) {
+  if (potion.prix > prixMax) {
+    nomPotion = potion.nom
+    prixMax = potion.prix
+  }
+  if (potion.stock > stockMax) {
+    stockMax = potion.stock
+  }
+  if (potion.stock > 0) {
+    stockSuperieurA0 += 1
+  }
+}
+
+console.log("Potion la plus chère :",nomPotion + " et coute",prixMax)
+console.log("Potion avec le plus de stock :",nomPotion + " avec un stock de",stockMax)
+console.log("Nombre de potions avec un stock supérieur à 0 :",stockSuperieurA0)
